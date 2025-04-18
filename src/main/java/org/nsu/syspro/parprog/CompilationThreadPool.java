b482dbaac8e247da7fe3d0cc4b3199ab5f4f1711
Sergey Novikov <isnov.contact@gmail.com>
2025-04-18 09:31:48 +0700

    fixup! CompilationThreadPool: prove CPU-bound-compilation works

--- src/main/java/org/nsu/syspro/parprog/CompilationThreadPool.java
+++ src/main/java/org/nsu/syspro/parprog/CompilationThreadPool.java
@@ -8,7 +8,9 @@ import org.nsu.syspro.parprog.solution.SolutionThread;
 import java.util.HashMap;
 import java.util.Optional;
 import java.util.concurrent.*;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class CompilationThreadPool {
@@ -16,7 +18,7 @@ public class CompilationThreadPool {
 
     private static HashMap<MethodID, Integer> compile_l1_counter = new HashMap<>();
     private static HashMap<MethodID, Integer> compile_l2_counter = new HashMap<>();
-    private static ReadWriteLock lock = new ReentrantReadWriteLock();
+    private static Lock lock = new ReentrantLock();
 
     private final ExecutorService service;
 
@@ -30,16 +32,12 @@ public class CompilationThreadPool {
             case L1 -> compile_l1_counter;
             case L2 -> compile_l2_counter;
         };
-        try {
-            lock.writeLock().lock();
-            if (counter.containsKey(methodID)){
-                var current = counter.get(methodID) + 1;
-                counter.put(methodID, current);
-            } else {
-                counter.put(methodID, 1);
-            }
-        } finally {
-            lock.writeLock().unlock();
+
+        if (counter.containsKey(methodID)){
+            var current = counter.get(methodID) + 1;
+            counter.put(methodID, current);
+        } else {
+            counter.put(methodID, 1);
         }
     }
 
@@ -48,15 +46,11 @@ public class CompilationThreadPool {
             case L1 -> compile_l1_counter;
             case L2 -> compile_l2_counter;
         };
-        try {
-            lock.writeLock().lock();
-            if (counter.containsKey(methodID)){
-                var current = counter.get(methodID) - 1;
-                assert current >= 0 : "Counter value of compiling method must be greater ot equal to 0";
-                counter.put(methodID, current);
-            }
-        } finally {
-            lock.writeLock().unlock();
+
+        if (counter.containsKey(methodID)){
+            var current = counter.get(methodID) - 1;
+            assert current >= 0 : "Counter value of compiling method must be greater ot equal to 0";
+            counter.put(methodID, current);
         }
     }
 
@@ -65,43 +59,48 @@ public class CompilationThreadPool {
             case L1 -> compile_l1_counter;
             case L2 -> compile_l2_counter;
         };
-        try {
-            lock.readLock().lock();
-            return Optional.ofNullable(counter.getOrDefault(methodID, null));
-        } finally {
-            lock.readLock().unlock();
-        }
+        return Optional.ofNullable(counter.getOrDefault(methodID, null));
     }
 
     public synchronized CompiledMethod compile(SolutionThread.CompilationLevel compilationLevel, MethodID methodID) {
         Callable<CompiledMethod> compileCallable = () -> switch (compilationLevel) {
             case L1: {
-                while (true){
-                    if (get(SolutionThread.CompilationLevel.L1, methodID).isEmpty() ||
-                            get(SolutionThread.CompilationLevel.L1, methodID).get() <= 1) {
-                        break;
-                    } else {
-                        Thread.sleep(100);
+                try {
+                    lock.lock();
+                    while (true) {
+                        if (get(SolutionThread.CompilationLevel.L1, methodID).isEmpty() ||
+                                get(SolutionThread.CompilationLevel.L1, methodID).get() <= 1) {
+                            break;
+                        } else {
+                            Thread.sleep(100);
+                        }
                     }
+                    increment(SolutionThread.CompilationLevel.L1, methodID);
+                    var code = compilationEngine.compile_l1(methodID);
+                    decrement(SolutionThread.CompilationLevel.L1, methodID);
+                    yield code;
+                } finally {
+                    lock.unlock();
                 }
-                increment(SolutionThread.CompilationLevel.L1, methodID);
-                var code =  compilationEngine.compile_l1(methodID);
-                decrement(SolutionThread.CompilationLevel.L1, methodID);
-                yield code;
             }
             case L2: {
-                while (true){
-                    if (get(SolutionThread.CompilationLevel.L2, methodID).isEmpty() ||
-                            get(SolutionThread.CompilationLevel.L2, methodID).get() <= 0) {
-                        break;
-                    } else {
-                        Thread.sleep(100);
+                try {
+                    lock.lock();
+                    while (true) {
+                        if (get(SolutionThread.CompilationLevel.L2, methodID).isEmpty() ||
+                                get(SolutionThread.CompilationLevel.L2, methodID).get() == 0) {
+                            break;
+                        } else {
+                            Thread.sleep(100);
+                        }
                     }
+                    increment(SolutionThread.CompilationLevel.L2, methodID);
+                    var code = compilationEngine.compile_l2(methodID);
+                    decrement(SolutionThread.CompilationLevel.L2, methodID);
+                    yield code;
+                } finally {
+                    lock.unlock();
                 }
-                increment(SolutionThread.CompilationLevel.L2, methodID);
-                var code =  compilationEngine.compile_l2(methodID);
-                decrement(SolutionThread.CompilationLevel.L2, methodID);
-                yield code;
             }
         };
 
